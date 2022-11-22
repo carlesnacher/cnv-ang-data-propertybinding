ab8a41281065bad819f955d9bc04f785a8640331
carlesnacher <carlesnacher@gmail.com>
2022-11-22 13:44:41 +0100

    Añado los articulos en app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cnv-ang-data-propertybinding';
+  articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+              {codigo:2, descripcion:'manzanas', precio:12.10},
+              {codigo:3, descripcion:'melon', precio:52.30},
+              {codigo:4, descripcion:'cebollas', precio:17},
+              {codigo:5, descripcion:'calabaza', precio:20},
+              ]
 }
