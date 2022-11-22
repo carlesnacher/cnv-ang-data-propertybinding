c61e16caa4791bd95f3fd71b576d514e5c280b0e
carlesnacher <carlesnacher@gmail.com>
2022-11-22 13:32:08 +0100

    Add implements OnInit listadoarticulos.component.ts

--- src/app/listadoarticulos/listadoarticulos.component.ts
+++ src/app/listadoarticulos/listadoarticulos.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listadoarticulos',
   templateUrl: './listadoarticulos.component.html',
   styleUrls: ['./listadoarticulos.component.css']
 })
-export class ListadoarticulosComponent {
+export class ListadoarticulosComponent implements OnInit {
 
+  @Input() datos: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
 }
